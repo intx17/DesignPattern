727d66d70308c9972886d84d3ddae72e5fbce45d
Tomohiro Yoshida <rollwithit0117@gmail.com>
2019-05-19 21:01:45 +0900

    add: SingletonPattern proj

---
+++ SingletonPattern/Models/RegisterNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingletonPattern.Models
+{
+    public class RegisterNote
+    {
+        private static RegisterNote _registerNote = new RegisterNote();
+        public List<string> Histories { get; set; } = new List<string>();
+
+        // Make Constructor private
+        private RegisterNote() { }
+
+        public static RegisterNote GetInstance() => _registerNote;
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SingletonPattern.Models;
+using Core.Extensions;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rn1 = RegisterNote.GetInstance();
+            var rn2 = RegisterNote.GetInstance();
+
+            Console.WriteLine(rn1.Equals(rn2));
+
+            rn1.Histories.Add("History1");
+
+            rn1.ConsoleLog("rn1");
+            rn1.ConsoleLog("rn2");
+        }
+    }
+}
